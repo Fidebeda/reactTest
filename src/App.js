adc82ffa1c695a6c9085094b287408ff928e3462
jan kopčák <honza.kopcak@gmail.com>
2022-04-02 17:52:50 +0200

    upravy2

--- src/App.js
+++ src/App.js
@@ -7,72 +7,123 @@ class App extends React.Component {
       super(props)
 
       this.state = {
-          dude: "Marcela the Elf",
+          newWho: "Marcela the Elf",
+          newWhat: "Best player on the world",
           characters: [
               {
                   id:1,
                   who:"Finn the Human",
                   what:"A silly kid who wants to become a hero",
-                  cool: 12
+                  cool: 69
               },
               {
                   id:2,
                   who:"Jake the Dog",
                   what:"Finn's best friend who wants to kill me",
-                  cool:42
+                  cool:9
               }
           ]
       }
   }
 
-  handleChange = e =>{
+  handleWho = e =>{
       this.setState({
-          dude: e.target.value
+          newWho: e.target.value
       })
       
   }
 
-  handleSubmit = event =>{
-      event.preventDefault()
+  handleWhat = e =>{
+    this.setState({
+        newWhat: e.target.value
+    })
+    
+}
 
-      const newDude = {
-        id:999,
-        who:this.state.dude,
-        what:this.state.dude,
-        cool: 15
+handleCool = dude => e =>{
+   const cool = +e.target.value
+
+   this.setState(state => { 
+       return { 
+           characters: state.characters.map(item =>
+            item === dude ? {...dude, cool } : item
+            )
+        }})
+    
+}
+
+removeDude = dude => {
+    this.setState(state =>{
+        return {
+            characters: state.characters.filter(item => item !== dude)
+        }
+    })
+}
+
+  handleSubmit = event =>{
+      if(event.key === 'Enter' && this.state.newWho && this.state.newWhat){
+        this.setState(state => {
+            const newDude = {
+                id:Math.max(...state.characters.map(d=>d.id) +1),
+                who:this.state.newWho,
+                what:this.state.newWhat,
+                cool: 15
+        
+              }
+    
+    
+               return {
+                    characters:[...state.characters, newDude] 
+                }})
+    
+        //   console.log([
+        //       ...this.state.characters, newDude
+    
+        //   ]);
+        
+        //   alert(this.state.dude)
+          
+        
+    }
 
       }
+      
+
+      
 
     //   this.setState({
     //       characters: [...this.state.characters, newDude]
     //   })
 
-      this.setState(state => {
-           return {
-                characters:[...state.characters, newDude] 
-            }})
-
-    //   console.log([
-    //       ...this.state.characters, newDude
-
-    //   ]);
-    
-    //   alert(this.state.dude)
       
-    
-}
 
   listOfDudes = () => {
-      return this.state.characters.map(dude => <li className={dude.who.split(" ").length < 3 ? "strong" : "gold"} key={dude.id}>{dude.who}
+      return this.state.characters.map(dude => (
+       <li className="dude" key={dude.id}>
+           <a className='ctrl' onClick={() =>{
+               this.removeDude(dude)
+           }}>x</a>
 
-      {dude.who.split(" ").length < 3 &&(
+           <article className={
+               dude.cool<10 ? 'faded' : dude.cool>50  ? 'gold' : ''
+           }>
+            {dude.who}
+            <span>{dude.what}</span>
+           </article>
+
+
+           <input className='ctrl' type="number" value={dude.cool} onChange={this.handleCool(dude)}></input>
+          
+
+      {/* {dude.who.split(" ").length < 3 &&(
         <small>
           <em> lol, short name</em>
         </small>
 
-      )}
+      )} */}
       
-      </li>)
+      </li>
+      ))
   }
 
 //   handleChange(e){
@@ -89,12 +140,18 @@ class App extends React.Component {
               <ul>{this.listOfDudes()}</ul>
 
 
-              <form className='add-new' onSubmit={this.handleSubmit}>
-                  <input type="text" value={this.state.dude} onChange={this.handleChange}></input>
+              <form className='add-new' onKeyPress={this.handleSubmit}>
+                  <input type="text" value={this.state.newWho} onChange={this.handleWho}></input>
+                  <input type="text" value={this.state.newWhat} onChange={this.handleWhat}></input>
 
               </form>
 
-              <p className='preview'>{this.state.dude}</p>
+              <p className='preview'>
+                  {this.state.newWho}
+                  <br></br>
+                  <small>{this.newWhat}</small>
+
+              </p>
               
               
           </div>
